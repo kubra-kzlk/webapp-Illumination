53784eb4e91ba6ec0cba2207b057cbf8235745c8
kubra kizilkilic <kubra.kizilkilic@student.ap.be>
2024-04-22 15:05:06 +0000

    fabrics

--- server.ts
+++ server.ts
@@ -1,37 +1,23 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import ejs from "ejs";
+import express, { Express } from "express";
 import { Lamp } from "./interface";
 import { Fabrikant } from "./interface";
-import path from "path";
-//import lampsDataJson from "JSON/lamps.json";
-//import fabricsDataJson from "./JSON/fabrikant.json";
 
 const app: Express = express();
-
-dotenv.config();
 
 app.set("view engine", "ejs"); // EJS als view engine
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));  //tell express to serve the content of public dir
-app.set('views', path.join(__dirname, "views"));
-app.set("port", 3000);
+app.use(express.static("public"));  //tell express to serve the content of public dir
+app.set("views", __dirname + "/views");
+app.set("port", process.env.PORT || 3000);
 
 
 // Parse JSON data
-const lampsData: Lamp[] = [];
-const fabricsData: Fabrikant[] = [];
+let lampsData: Lamp[] = [];
+let fabricsData: Fabrikant[] = [];
 
-//als je / intypt dan word je nr de home pag verwezen
-app.get("/", (req: Request, res: Response) => {
-  res.render("index", {
-    title: "Home",
-    message: "Welcome !"
-  });
-});
 
-app.get("/lamps", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   //zoeken
   const searchQuery = typeof req.query.q === "string" ? req.query.q.toLowerCase() : "";
   const sortField = typeof req.query.sortField === "string" ? req.query.sortField : "naam";
@@ -107,7 +93,7 @@ app.get("/lamps", (req: Request, res: Response) => {
     },
   ];
 
-  res.render("lamps", {
+  res.render("index", {
     lamps: sortedLamps,
     sortFields: sortFields,
     sortDirections: sortDirections,
@@ -117,111 +103,8 @@ app.get("/lamps", (req: Request, res: Response) => {
   });
 });
 
-app.get("lamps/:id", (req: Request, res: Response) => {
-
-  const lamp = lampsData.find((c: Lamp) => c.id.toString() === req.params.id);
-  if (!lamp) {
-    return res.status(404).send("lamp not found");
-  }
-  res.render("lampDetail", { lamp });
-});
-
-app.get("/fabrics", (req: Request, res: Response) => {
-  //zoeken
-  const searchQuery = typeof req.query.q === "string" ? req.query.q.toLowerCase() : "";
-  const sortField = typeof req.query.sortField === "string" ? req.query.sortField : "naam";
-  const sortDirection = typeof req.query.sortDirection === "string" ? req.query.sortDirection : "asc";
-  let filteredData = fabricsData;
-
-  let sortedFAbrics = [...lampsData].sort((a, b) => {
-    if (sortField === "naam") {
-      return sortDirection === "asc"
-        ? a.naam.localeCompare(b.naam)
-        : b.naam.localeCompare(a.naam);
-    } else if (sortField === "prijs") {
-      return sortDirection === "asc" ? a.prijs - b.prijs : b.prijs - a.prijs;
-    } else if (sortField === "id") {
-      return sortDirection === "asc" ? a.id - b.id : b.id - a.id;
-    } else if (sortField === "kleur") {
-      return sortDirection === "asc"
-        ? a.kleur.localeCompare(b.kleur)
-        : b.kleur.localeCompare(a.kleur);
-    } else if (sortField === "actief") {
-      return sortDirection === "asc"
-        ? Number(a.actief) - Number(b.actief)
-        : Number(b.actief) - Number(a.actief);
-    }
-    else {
-      return 0;
-    }
-  });
-
-  if (searchQuery) {
-    filteredData = filteredData.filter((fabric) =>
-      fabric.naam.toLowerCase().includes(searchQuery)
-    );
-  }
-  const sortFields = [
-    {
-      value: "naam",
-      text: "Naam",
-      selected: sortField === "naam" ? "selected" : "",
-    },
-    {
-      value: "prijs",
-      text: "Prijs",
-      selected: sortField === "prijs" ? "selected" : "",
-    },
-    {
-      value: "id",
-      text: "Id",
-      selected: sortField === "id" ? "selected" : "",
-    },
-    {
-      value: "kleur",
-      text: "Kleur",
-      selected: sortField === "kleur" ? "selected" : "",
-    },
-    {
-      value: "actief",
-      text: "Actief",
-      selected: sortField === "actief" ? "selected" : "",
-    }
-  ];
-
-  const sortDirections = [
-    {
-      value: "asc",
-      text: "Ascending",
-      selected: sortDirection === "asc" ? "selected" : "",
-    },
-    {
-      value: "desc",
-      text: "Descending",
-      selected: sortDirection === "desc" ? "selected" : "",
-    },
-  ];
-
-  res.render("fabrics", {
-    fabrics: sortedFAbrics,
-    sortFields: sortFields,
-    sortDirections: sortDirections,
-    sortField: sortField,
-    sortDirection: sortDirection,
-    searchQuery
-  });
-});
-
-app.get("/fabrics/:id", async (req, res) => {
-  const fabricId = parseInt(req.params.id);
-  const fabric = fabricsData.find(fabric => fabric.id === fabricId);
-
-  if (!fabric) {
-    return res.status(404).send("Fabric not found");
-  }
-
-  const relatedLamps = lampsData.filter(lamp => lamp.id === fabricId);
-  res.render("fabricDetail", { fabric, relatedLamps });
+app.get('/fabrics', (req, res) => {
+  res.render('fabrics');
 });
 
 //start server
@@ -229,5 +112,6 @@ app.listen(app.get("port"), async () => {
   let response = await fetch(
     "https://raw.githubusercontent.com/kubra-kzlk/lamps/main/lamps.json"
   );
+  lampsData = await response.json();
   console.log("[server] http://localhost:" + app.get("port"));
 });
