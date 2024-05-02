3c01acc92cfadab6fd5b576940c872b53a1b5768
kubra kizilkilic <kubra.kizilkilic@student.ap.be>
2024-05-02 16:45:09 +0000

    lampEdit pag form aanmaken

--- index.ts
+++ index.ts
@@ -180,6 +180,7 @@ async function main() {
     if (!lampsCheck) {
       //deel 3: Data naar MongoDB schrijven
       //fetch from API
+      //Zorg ervoor dat je alle data dat je via de fetch API ophaalt in MongoDB naar mongoDB schrijft. Kijk bij de opstart van de applicatie altijd eerst of er data in de database zit. Als er geen data in de database zit, dan moet je de data ophalen via de fetch API en in de database schrijven. Als er data inzit gebruik je de data die afkomstig is uit de MongoDB database.
       const lampResponse = await fetch('https://raw.githubusercontent.com/kubra-kzlk/lamps/main/lamps.json');
       const lampsdata = await lampResponse.json();
       lampsData.push(...lampsdata);
@@ -194,6 +195,7 @@ async function main() {
       await db.collection("Fabrikant").insertMany(fabrikantdata);
     }
     //deel 3:Data ophalen uit MongoDB
+    //Zorg ervoor dat alle endpoints worden aangepast zodat de data uit de MongoDB database wordt gehaald ipv de data die je via de fetch API ophaalt.
     //fetch from db
     lampsData = await db.collection("Lamps").find<Lamp>({}).toArray();
     fabricsData = await db.collection("Fabrikant").find<Fabrikant>({}).toArray();
